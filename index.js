8970fbaeab2e3d8f8c355bbb091f46ba138882bb
RaymondwChen2 <raymondwchen2@gmail.com>
2023-01-03 07:52:46 -0800

    render a navbar using JSX

--- index.js
+++ index.js
@@ -10,13 +10,47 @@
 //     document.getElementById('root')
 // )
 
+
+// Render a component ###################################
+
+
+// function MainContent(){
+//     return <h1>I'm learning React!</h1>
+// }
+
+// ReactDOM.render(
+//     <div>
+//         <MainContent/>
+//     </div>,
+//         document.getElementById('root')
+// )
+
+
+// Creating a nav bar using JSX - using 1 parent ###########
+
 function MainContent(){
     return <h1>I'm learning React!</h1>
+}
+
+function NavBar(){
+    return (
+        <div>
+            <h1>Sushi Heaven</h1>
+            <nav>
+                <ul>
+                    <li>Pricing</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </div>
+    )
 }
 
 ReactDOM.render(
     <div>
         <MainContent/>
+        <NavBar/>
     </div>,
         document.getElementById('root')
 )
