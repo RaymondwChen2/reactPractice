a54759ee38c4c3226f11049e522f2632edd63fb6
RaymondwChen2 <raymondwchen2@gmail.com>
2023-01-03 07:27:57 -0800

    Render a React component

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
+// ReactDOM.render(
+//     <div>
+//         <h1>Fruit List</h1>
+//         <ul>
+//             <li>Banana</li>
+//             <li>Apple</li>
+//             <li>Grapes</li>
+//         </ul>
+//     </div>,
+//     document.getElementById('root')
+// )
+
+function MainContent(){
+    return <h1>I'm learning React!</h1>
+}
+
 ReactDOM.render(
     <div>
-        <h1>Fruit List</h1>
-        <ul>
-            <li>Banana</li>
-            <li>Apple</li>
-            <li>Grapes</li>
-        </ul>
+        <MainContent/>
     </div>,
-    document.getElementById('root')
+        document.getElementById('root')
 )
