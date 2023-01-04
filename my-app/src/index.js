912e6358762e0de23606b5f89bb6665289d5928f
RaymondwChen2 <raymondwchen2@gmail.com>
2023-01-04 06:48:47 -0800

    Created a board with clickable squares

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,10 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+
+
 class Board extends React.Component {
     renderSquare(i) {
         return <Square value={i}/>
     }
+
+    render() {
+        const status = "Next Player: X"
+
+        return (
+            <div>
+                <div className='status'> {status}</div>
+                <div className='board-row'>
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className='board-row'>
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div><div className='board-row'>
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        )
+    }
+
 }
 
 class Square extends React.Component {
@@ -17,3 +44,22 @@ class Square extends React.Component {
         )
     }
 }
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className='game'>
+                <div className='game-board'>
+                    <Board/>
+                </div>
+                <div className='game-info'>
+                    <div>{/*status*/}</div>
+                    <div>{/*TODO*/}</div>
+                </div>
+            </div>
+        );
+    }
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Game/>)
