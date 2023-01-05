6b33fdcb9af368d1b9b6c5600888372f8163d945
RaymondwChen2 <raymondwchen2@gmail.com>
2023-01-05 10:39:00 -0800

    Add constructor to Board component

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,6 +5,15 @@ import './index.css';
 
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null)
+        }
+    }
+
+
+
     renderSquare(i) {
         return <Square value={i}/>
     }
@@ -16,18 +25,18 @@ class Board extends React.Component {
             <div>
                 <div className='status'> {status}</div>
                 <div className='board-row'>
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
+                    {this.renderSquare()}
+                    {this.renderSquare()}
+                    {this.renderSquare()}
                 </div>
                 <div className='board-row'>
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
+                    {this.renderSquare()}
+                    {this.renderSquare()}
+                    {this.renderSquare()}
                 </div><div className='board-row'>
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
+                    {this.renderSquare()}
+                    {this.renderSquare()}
+                    {this.renderSquare()}
                 </div>
             </div>
         )
