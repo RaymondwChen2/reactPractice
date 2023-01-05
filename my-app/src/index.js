6970862a1d75258fe757c8096616b85a8c6e1131
RaymondwChen2 <raymondwchen2@gmail.com>
2023-01-05 11:02:53 -0800

    Initialize handleClick function

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -12,10 +12,19 @@ class Board extends React.Component {
         }
     }
 
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
 
 
     renderSquare(i) {
-        return <Square value={i}/>
+        return(
+            <Square value={this.state.squares[i]}
+            onClick={() => this.handleClick(i)}
+            />
+        )
     }
 
     render() {
@@ -45,18 +54,12 @@ class Board extends React.Component {
 }
 
 class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null
-        }
-    }
-
-
     render() {
         return (
-            <button className='square' onClick={() => this.setState({value: "X"})}>
-                {this.state.value}
+            <button className='square' 
+            onClick={() => this.props.onClick({value: "X"})}
+            >
+            {this.props.value}
             </button>
         )
     }
